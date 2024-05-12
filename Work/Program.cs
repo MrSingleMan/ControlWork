b91c6d7e92ea80a004661f675c7eae3b7447385f
Alexander <n00mer00k@gmail.com>
2024-05-12 15:53:10 +0400

    Add README

--- Work/Program.cs
+++ Work/Program.cs
@@ -8,35 +8,35 @@
 // [“Russia”, “Denmark”, “Kazan”] → []
 
 string[] CreateArray(string pols) //Функция создания массива из строки
-    {
-        string[] ArrayPols = pols.Split(' '); // Разбиваем введенную строку на элементы массива по пробелам и сохраняем в массив
-        return ArrayPols;
-    }
+{
+    string[] ArrayPols = pols.Split(' '); // Разбиваем введенную строку на элементы массива по пробелам и сохраняем в массив
+    return ArrayPols;
+}
 
 void PrintArray(string[] ArrayPols) //Функция печати массива
+{
+    Console.WriteLine();
+    foreach (string word in ArrayPols)
     {
-        Console.WriteLine();
-        foreach (string word in ArrayPols)
-        {
-            Console.Write(word + " ");
-        }
-        Console.WriteLine();
+        Console.Write(word + " ");
     }
- 
+    Console.WriteLine();
+}
+
 string[] ArrayLess3(string[] ArrayPols)
+{
+    string[] newArray = new string[ArrayPols.Length]; // Создаем новый массив такой же длины, как оригинальный
+    int j = 0;                                        // Индекс для нового массива
+    foreach (string word in ArrayPols)
     {
-        string[] newArray = new string[ArrayPols.Length]; // Создаем новый массив такой же длины, как оригинальный
-        int j = 0;                                        // Индекс для нового массива
-        foreach (string word in ArrayPols)
+        if (word.Length <= 3)
         {
-            if (word.Length <= 3)
-            {
-                newArray[j] = word;
-                j++;
-            }
+            newArray[j] = word;
+            j++;
         }
-        return newArray;
     }
+    return newArray;
+}
 
 System.Console.WriteLine("Введите строку, разделяя слова пробелами:");
 string pols = Console.ReadLine(); // Пользователь задаёт строку из слов
